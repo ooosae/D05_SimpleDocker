bd53afbabc0356b0aebd2e3d0ac4d17faf4a14ee
N <thecatty@yandex.ru>
2024-01-21 13:58:50 +0300

    all

---
+++ src/server/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <fcgi_stdio.h>
+
+int main() {
+    while (FCGI_Accept() >= 0) {
+        printf("Content-type: text/html\r\n\r\n");
+        printf("<html><head><title>Hello World</title></head><body>");
+        printf("<h1>Hello World!</h1>");
+        printf("</body></html>");
+    }
+    return 0;
+}
